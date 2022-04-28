96246c770baec3c69fb30c51d2dca2c1f74b2a21
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2022-04-28 16:45:38 +0100

    Create Compile.h

---
+++ src/Compile.h
@@ -0,0 +1,12 @@
+#ifndef STDLIB
+#define STDLIB
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#endif
+
+char compile(const uint8_t* FileLocation);
