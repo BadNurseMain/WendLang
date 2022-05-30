3b4ea50b08ae49a17d870c8635d9fa5d6b4e6fbb
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2022-05-30 18:58:17 +0100

    Added better Error Checking.

--- src/Compiler/Func.h
+++ src/Compiler/Func.h
@@ -19,6 +19,15 @@
         * Check return type.
 */
 
+//Function Error Codes.
+#define FN_CALL_INVALID (uint8_t) 0xA0
+#define FN_CALL_UNKNOWN (uint8_t) 0xA1
+
+#define FN_LACK_PARAMS (uint8_t) 0xA2
+#define FN_DIFF_PARAM (uint8_t) 0xA3
+
+#define FN_RETURN_INVAL (uint8_t) 0xA4
+
 uint8_t makeFunction(uint32_t Location);
 
 uint8_t callFunction(uint8_t* FunctionName, uint8_t ReturnRegister, uint16_t ParameterCount, uint8_t** Buffer);
