0b4d7ad301e9eaf86668e91d4afca1c5f98aa464
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2022-06-09 01:10:02 +0100

    Update Arith.h

--- src/Compiler/Arith.h
+++ src/Compiler/Arith.h
@@ -22,11 +22,21 @@
 #define ARTH_INVALID_SIZE (uint8_t) 0xC0
 #define ARTH_UNKNOWN_VAR (uint8_t) 0xC1
 
+#define ARTH_TYPE_STACK (uint8_t) (1 << 7)
+#define ARTH_TYPE_STORE_A (uint8_t) 1
+#define ARTH_TYPE_STORE_B (uint8_t) 2
+
+#ifndef NAMESTRUCT
+#define NAMESTRUCT
+
 typedef struct
 {
     uint8_t* Name;
     uint8_t Type;
     uint32_t StackOffset;
 } LocalNameStruct;
+#endif
+
+uint32_t writeConditionalOperations(uint8_t* FunctionName, uint32_t StartLocation, LocalNameStruct* Variables, uint32_t VariableCount, uint32_t ConditionalCount, uint8_t OptionalParam);
 
 uint32_t writeArithmeticOperations(uint8_t TabOffset, uint32_t StartLocation, LocalNameStruct* Variables, uint32_t VariableCount, uint8_t OptionalParam);
