99176778a8855b083f147c3d057845b8060dca95
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2022-06-22 14:24:16 +0100

    Create Main.c

---
+++ src/Compiler/Frontend/Main.c
@@ -0,0 +1,34 @@
+#include "Compile.h"
+
+int main(int argc, char* argv[])
+{
+    uint8_t OutputLocation[64] = {0};
+    uint8_t InputLocation[64] = {0};
+
+    for(uint8_t x = 1; x < argc; x++)
+    {
+        if(argv[x][0] == '-')
+        {
+            if(!strcmp(argv[x], "-o"))
+            {
+                strncpy(OutputLocation, argv[++x], 64);
+                continue;
+            }
+            return 1;
+        }
+
+        if(!InputLocation[0]) strcpy(InputLocation, argv[x]);
+        else
+        {
+            printf("Multiple Files Defined as Input \n");
+            return 1;
+        }
+    }
+
+    if(!InputLocation[0]) return 2;
+
+    if(OutputLocation[0]) compile(InputLocation, OutputLocation);
+    else compile(InputLocation, 0);
+    
+    return 0;    
+}
